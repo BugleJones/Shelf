3e06f7edf516b54b1c0add6636dcddc258c9de20
BugleJones <spencerjhamilton@gmail.com>
2017-09-28 01:44:46 +0000

    Created new user table in db and updated seed data

---
+++ db/migrations/20170928003111_shelf_users_table.js
@@ -0,0 +1,20 @@
+
+exports.up = function(knex, Promise) {
+  return Promise.all([
+    knex.schema.createTable('users', function (table){
+      table.increments();
+      table.string('first_name');
+      table.string('last_name');
+      table.string('email');
+      table.string('username');
+      table.string('password');
+      table.timestamps(true, true);
+    })
+  ])
+};
+
+exports.down = function(knex, Promise) {
+  return Promise.all([
+  knex.schema.dropTable('users')
+  ])
+};
--- db/seeds/users.js
+++ db/seeds/users.js
@@ -2,9 +2,9 @@ exports.seed = function(knex, Promise) {
   return knex('users').del()
     .then(function () {
       return Promise.all([
-        knex('users').insert({id: 1, name: 'Alice'}),
-        knex('users').insert({id: 2, name: 'Bob'}),
-        knex('users').insert({id: 3, name: 'Charlie'})
+        knex('users').insert({first_name: 'Alice', last_name: 'Murphy', email: 'alicemurphy@email.com', username: 'MurphysLaw', password: 'murphy'}),
+        knex('users').insert({first_name: 'Bob', last_name: 'Jones', email: 'bobsojones@email.com', username: 'YoBob', password: 'jones'}),
+        knex('users').insert({first_name: 'Charlie', last_name: 'Lankaster', email: 'charlie@email.com', username: 'CharlieDontSurf', password: 'lankaster'})
       ]);
     });
 };
