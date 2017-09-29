959c2194ff8ca6562d63505b0874ad557895f198
kevinmore80 <kevinmore@outlook.com>
2017-09-29 04:12:16 +0000

    Search function in beta

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,28 @@ $(() => {
       $("<div>").text(user.username).appendTo($("body"));
     }
   });;
+
+ //Searches database
+  const searchRes = (searchFormData) => {
+    console.log(searchFormData);
+    $.ajax({
+      method: "GET",
+      url: `/api/search/${searchFormData}`,
+    }).done( (result) => {
+      $("<h2>").text("Results:").appendTo($("#search_results"));
+      result.rows.forEach( (row) => {
+        $("<div>").text(row.title).appendTo($("#search_results"));
+      })
+    })
+      .fail( (e) => {
+        console.error(e);
+      })
+  }
+
+    //Searches database for resources
+  $('#search').on('submit', () => {
+    event.preventDefault();
+    searchRes($("#query").val());
+  });
+
 });
